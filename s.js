cb97f4f4b24e4f33c933a571f2ff9469a3fb4359
Ilyas Seisov <ilyasseisov@gmail.com>
2023-05-21 17:49:23 +0500

    initial commit

---
+++ s.js
@@ -0,0 +1,88 @@
+// Variables
+// DOM
+const titleInput = document.querySelector('#title-input');
+const permalinkInput = document.querySelector('#permalink-input');
+const urlInput = document.querySelector('#url-input');
+const permalinkLengthText = document.querySelector('#permalink-length');
+const eraseTitleBtn = document.querySelector('#erase-title');
+const copyPermalink = document.querySelector('#copy-permalink');
+const copyURL = document.querySelector('#copy-url');
+const copyStatusPermalink = document.querySelector('#copy-status-permalink');
+const copyStatusURL = document.querySelector('#copy-status-url');
+//
+const domainName = 'https://zen-gardener.com/';
+// Functions
+// title parse
+const parseTitle = () => {
+  const originalString = titleInput.value;
+  const cleanString = originalString
+    .replaceAll(':', '')
+    .replaceAll('?', '')
+    .replaceAll('(', '')
+    .replaceAll(')', '')
+    .replaceAll('.', '')
+    .replaceAll(',', '')
+    .replaceAll(';', '')
+    .replaceAll('-', '')
+    .replaceAll('_', '')
+    .replaceAll('=', '')
+    .replaceAll('+', '');
+  const wordsArray = cleanString.split(' ');
+  const cleanWordsArray = wordsArray.filter((word) => {
+    return word !== '';
+  });
+  const lowerCaseWordsArray = cleanWordsArray.map((word) => {
+    return word.toLowerCase();
+  });
+  // permalink generation
+  const permalinkString = lowerCaseWordsArray.join('-');
+  permalinkInput.value = permalinkString;
+  permalinkLengthText.textContent = permalinkInput.value.length;
+  console.log(permalinkInput.value.length);
+  permalinkLengthCheck();
+  // url generation
+  urlInput.value = `${domainName}${permalinkString}`;
+};
+
+// permalink length
+const permalinkLengthCheck = () => {
+  // error status
+  if (permalinkInput.value.length >= 75) {
+    permalinkInput.classList.add('error');
+  } else {
+    permalinkInput.classList.remove('error');
+  }
+  // length value display
+  permalinkLengthText.textContent = permalinkInput.value.length;
+  // url generation
+  urlInput.value = `${domainName}${permalinkInput.value}`;
+};
+
+// erase title
+const eraseTitle = () => {
+  titleInput.value = '';
+  permalinkInput.value = '';
+  urlInput.value = '';
+  permalinkLengthText.textContent = '0';
+};
+
+// copy to clipboard
+const copyToClipboard = (input, badge) => {
+  navigator.clipboard.writeText(input.value);
+  badge.classList.add('active');
+
+  setTimeout(() => {
+    badge.classList.remove('active');
+  }, 1200);
+};
+
+// Event Listeners
+titleInput.addEventListener('input', parseTitle);
+permalinkInput.addEventListener('input', permalinkLengthCheck);
+eraseTitleBtn.addEventListener('click', eraseTitle);
+copyPermalink.addEventListener('click', () => {
+  copyToClipboard(permalinkInput, copyStatusPermalink);
+});
+copyURL.addEventListener('click', () => {
+  copyToClipboard(urlInput, copyStatusURL);
+});
